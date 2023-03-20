bc7b85e3b009c4fbc0fb558902e10d9da5c8aed3
Azhar <bcasazharuddin@gmail.com>
2023-03-20 18:10:09 +0530

    change src and test js file

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <h1>This was changed by Arzoo Jangra</h1>
-      <h1>Mohd AzhauddinY=</h1>
+      <h1>Mohd Azhauddin</h1>
       <h1>Ankuj Pandey</h1>
     </>
   );
---
+++ src/test.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function test(props) {
+  return (
+    <div>
+      <h1>adajfasjkfasjkgakj</h1>
+    </div>
+  );
+}
+
+export default test;
